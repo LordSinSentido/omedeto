1206a5182465056bc5a9ed30f15953d3091ebab6
LordSinSentido <misogu@live.com>
2021-10-29 15:02:44 -0500

    Se agregó funcionalidad a la navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'principal', pathMatch: 'full'},
   {path : 'principal', component: PrincipalComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: '**', redirectTo: 'principal', pathMatch: 'full'}
 ];
 
