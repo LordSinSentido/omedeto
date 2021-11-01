eb27efe3831d36e2a579f72a479437c8c8f99dc0
LordSinSentido <misogu@live.com>
2021-11-01 12:24:16 -0600

    Se creó la ruta de recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { RecipeComponent } from './recipe/recipe.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path : 'principal', component: PrincipalComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'recipe', component: RecipeComponent},
   {path: '**', redirectTo: 'principal', pathMatch: 'full'}
 ];
 
