4bddf4ac3d755c3fdec2298823f9006146edd7da
LordSinSentido <misogu@live.com>
2021-11-01 15:03:27 -0600

    Se modificó la sintaxis

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -18,27 +18,31 @@ export class AuthService {
     });
   }
 
-  registarUsuario(correo: string, contrasenna: string) {
+  registarUsuario(correo: string, contrasenna: string): Promise<any> {
+    return this.autenticacion.createUserWithEmailAndPassword(correo, contrasenna);
+    /*
     this.autenticacion.createUserWithEmailAndPassword(correo, contrasenna).then(usuario => {
       this.snackbar.open("¡Listo!, ahora puedes iniciar sesión", "", {duration: 3000});
       this.redireccionar.navigate(['/login']);
       this.autenticacion.signOut();
     }).catch(error => {
       this.snackbar.open(error.message, "Aceptar", {duration: 7000});
-    });
+    });*/
   }
 
   comprobarEstado() {
-    const usuario = this.autenticacion.currentUser;
-    return usuario;
+    return this.autenticacion.currentUser;
   }
 
-  cerrarSesion() {
+  cerrarSesion(): Promise<any> {
+    return this.autenticacion.signOut();
+    /*
     this.autenticacion.signOut().then(() => {
       this.snackbar.open("Cerraste sesión", "", {duration: 3000});
       this.redireccionar.navigate(['/']);
     }).catch(error => {
       this.snackbar.open(error.message, "Aceptar", {duration: 7000});
-    });
+    });*/
+    
   }
 }
