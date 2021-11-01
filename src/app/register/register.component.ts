8b64b31c9a6cbe6ca4a3b0071d8c389ba267847b
LordSinSentido <misogu@live.com>
2021-11-01 15:04:17 -0600

    Se completó el modulo de register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { StorageService } from '../services/storage.service';
 import { AuthService } from './../services/auth.service';
 
 @Component({
@@ -10,21 +12,71 @@ import { AuthService } from './../services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  FormularioDeRegistro = new FormGroup({
-    correo: new FormControl(''),
-    contrasenna: new FormControl('')
-  });
-
+  formularioDeRegistro: FormGroup;
   cargando: boolean = false;
+  fotoDePerfil: any;
+  progresoDeCarga: number = 0;
 
-  constructor(private ServicioDeAutenticacion: AuthService, private redireccionar: Router) { }
+  constructor(private ServicioDeAutenticacion: AuthService, private ServicioDeAlmacenamiento: StorageService, private datosDelUsuario: FormBuilder, private redireccionar: Router, private snackbar: MatSnackBar) {
+    this.formularioDeRegistro = this.datosDelUsuario.group({
+      nombreDelUsuario: ['', Validators.required],
+      correo: ['', Validators.email],
+      contrasenna: ['', Validators.required],
+      confirmacionContrasenna: ['', Validators.required],
+      fotoUrl: ['']
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  obtenerImagen(evento:any){
+    this.fotoDePerfil = evento.target?.files[0];
+  }
+
   registrar() {
-    const {correo, contrasenna, nombre} = this.FormularioDeRegistro.value;
-    this.ServicioDeAutenticacion.registarUsuario(correo, contrasenna);
-    this.cargando = true;
+
+    // Comprueba las contrasennas
+    if(this.formularioDeRegistro.value.contrasenna != this.formularioDeRegistro.value.confirmacionContrasenna) {
+      this.snackbar.open("Las contraseñas no coinciden, revisalas", "Aceptar", {duration: 7000});
+      return
+    }
+
+    // Genera el usuario en Firebase
+    this.ServicioDeAutenticacion.registarUsuario(this.formularioDeRegistro.value.correo, this.formularioDeRegistro.value.contrasenna).then(usuarioCreado => {
+
+      // Carga de la imagen de perfil
+      this.ServicioDeAlmacenamiento.cargarFotoDePerfil(usuarioCreado.uid).put(this.fotoDePerfil).on(
+        'state_changed',
+        snapshot => {
+          let progresoDeCarga = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+          this.progresoDeCarga = progresoDeCarga;
+        },
+        error => {
+          this.snackbar.open(error.message, "Aceptar", {duration: 7000});
+        },
+        () => { /// <---- Hay que revisar esta parte, no está retornando la url de la imagen
+          this.ServicioDeAlmacenamiento.cargarFotoDePerfil(usuarioCreado.uid).put(this.fotoDePerfil).snapshot.ref.getDownloadURL().then(url => {
+            this.formularioDeRegistro.value.fotoUrl = url;
+          }).catch(error => {
+            console.log(error.message, 'Foto De Pefil');
+          })
+        }
+      );
+
+
+      usuarioCreado.user.updateProfile({
+        uid: this.formularioDeRegistro.value.nombreDelUsuario,
+        displayName: this.formularioDeRegistro.value.nombreDelUsuario,
+        photoURL: this.formularioDeRegistro.value.fotoUrl
+      })
+
+      this.snackbar.open("¡Listo!, ahora puedes iniciar sesión", "", {duration: 3000});
+      this.redireccionar.navigate(['/login']);
+      this.ServicioDeAutenticacion.cerrarSesion();
+
+    }).catch(error => {
+      this.snackbar.open(error.message, "Aceptar", {duration: 7000});
+    });
   }
 }
