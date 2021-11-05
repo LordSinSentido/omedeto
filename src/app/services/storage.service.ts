85b38c3031b00dc9b2fe94b19eb3716dbcd1a02a
LordSinSentido <misogu@live.com>
2021-11-05 14:42:07 -0600

    Se agregaron cargar y eliminar foto de perfil y de receta

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -8,15 +8,24 @@ export class StorageService {
 
   constructor(private almacenamiento: AngularFireStorage ) { }
   
-  cargarFotoDePerfil(archivo: File, uid: string) {
-    const path = this.almacenamiento.storage.ref(`fotosDePerfil/${uid}`);
+  cargarFotoDePerfil(archivo: File, idUsuario: string) {
+    const path = this.almacenamiento.storage.ref(`fotosDePerfil/${idUsuario}`);
     const subir = path.put(archivo);
     return subir;
   }
 
-  cargarFotoDeReceta(archivo: File, receta: string, uid: string) {
-    const path = this.almacenamiento.storage.ref(`fotosDeReceta/${uid}/${receta}`);
+  eliminarFotoPerfil(idUsuario: string): Promise<any> {
+    return this.almacenamiento.storage.ref(`fotosDePerfil/${idUsuario}`).delete();
+  }
+  
+  cargarFotoDeReceta(archivo: File, idReceta: string, idUsuario: string) {
+    const path = this.almacenamiento.storage.ref(`fotosDeReceta/${idUsuario}/${idReceta}`);
     const subir = path.put(archivo);
     return subir;
   }
+
+  eliminarFotoDeReceta(idUsuario: string, idReceta: string): Promise<any> {
+    return this.almacenamiento.storage.ref(`fotosDeReceta/${idUsuario}/${idReceta}`).delete();
+  }
+
 }
