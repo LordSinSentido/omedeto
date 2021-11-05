7038ec8267a6d9702e5d8159aae5f5d5a4c6c3b7
LordSinSentido <misogu@live.com>
2021-11-05 14:43:35 -0600

    Se agregó editar foto, obtener todas, solo las mias o una en especifica y eliminar receta

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -13,16 +13,24 @@ export class FirestoreService {
     return this.firestore.collection('recipes').add(receta);
   };
 
-  editarFoto(idReceta: string, recetaActualizada: any) {
+  editarFoto(idReceta: string, recetaActualizada: any): Promise<any> {
     return this.firestore.collection('recipes').doc(idReceta).update(recetaActualizada);
   }
 
   obtenerTodasLasRecetas(): Observable<any>{
-    return this.firestore.collection('recipes').snapshotChanges();
+    return this.firestore.collection('recipes', ref => ref.orderBy('fechaDeActualizacion', 'desc')).snapshotChanges();
   }
 
-  obtenerReceta(id: string) {
+  obtenerMisRecetas(idUsuario: string): Observable<any> {
+    return this.firestore.collection('recipes', ref => ref.orderBy('fechaDeActualizacion', 'desc').where('autorID', '==', idUsuario)).snapshotChanges();
+  }
+
+  obtenerReceta(id: string): Observable<any> {
     return this.firestore.collection('recipes').doc(id).snapshotChanges();
+  }
+
+  eliminarReceta(idReceta: string): Promise<any> {
+    return this.firestore.collection('recipes').doc(idReceta).delete();
   }
 
 }
