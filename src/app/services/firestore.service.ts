210b750a1f8a2cf0c29d2241cad63f2b24deabd6
LordSinSentido <misogu@live.com>
2021-11-23 17:45:57 -0600

    Se agregó funciones correspondientes a la información de los usuarios

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -9,16 +9,28 @@ export class FirestoreService {
 
   constructor(private firestore: AngularFirestore) { }
 
+  agregarUsuario(uid: string, usuario: any): Promise<any> {
+    return this.firestore.collection('usersData').doc(uid).set(usuario);
+  }
+
   agregarReceta(receta: any): Promise<any> {
     return this.firestore.collection('recipes').add(receta);
-  };
+  }
 
   editarFoto(idReceta: string, recetaActualizada: any): Promise<any> {
     return this.firestore.collection('recipes').doc(idReceta).update(recetaActualizada);
   }
 
+  obtenerTipoDeUsuario(uid: string) {
+    return this.firestore.collection('usersData').doc(uid).get();
+  }
+
   obtenerTodasLasRecetas(): Observable<any>{
     return this.firestore.collection('recipes', ref => ref.orderBy('fechaDeActualizacion', 'desc')).snapshotChanges();
+  }
+
+  obtenerTodosLosUsuarios(): Observable<any> {
+    return this.firestore.collection('usersData', ref => ref.orderBy('nombreDelUsuario', 'asc')).snapshotChanges();
   }
 
   obtenerMisRecetas(idUsuario: string): Observable<any> {
@@ -33,8 +45,16 @@ export class FirestoreService {
     return this.firestore.collection('recipes').doc(idReceta).delete();
   }
 
+  eliminarUsuario(uid: string): Promise<any> {
+    return this.firestore.collection('usersData').doc(uid).delete();
+  }
+
   actualizarReceta(idReceta: string, receta: any) {
     return this.firestore.collection('recipes').doc(idReceta).update(receta);
+  }
+
+  actualizarTipo(uid: string, datos: any) {
+    return this.firestore.collection('usersData').doc(uid).update(datos);
   }
 
 }
